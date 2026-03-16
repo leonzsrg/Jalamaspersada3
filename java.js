3b5dc625b99a390e14b43cabf0c6b77e988e846e
Rhenaldinata Leonoreza <137272518+leonzsrg@users.noreply.github.com>
2026-03-17 00:12:38 +0700

    Update java.js

--- java.js
+++ java.js
@@ -1,37 +1,35 @@
-/*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
 
 if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+navToggle.addEventListener('click',()=>{
+navMenu.classList.add('show-menu')
+})
 }
 
 if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+navClose.addEventListener('click',()=>{
+navMenu.classList.remove('show-menu')
+})
 }
 
-/*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+navMenu.classList.remove('show-menu')
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
+origin:'top',
+distance:'60px',
+duration:2000,
+delay:300
 })
 
-sr.reveal(`.home__data`)
-sr.reveal(`.home__img`, {delay: 500, origin: 'right'})
-sr.reveal(`.layanan__card`, {interval: 100})
+sr.reveal('.home__data')
+sr.reveal('.home__img',{delay:500})
+sr.reveal('.layanan__card',{interval:200})
